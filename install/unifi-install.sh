667e1bda72ef8ac44ee154c1dbb38f8ef2d154b8
chunky1 <32406050+chunky1@users.noreply.github.com>
2024-05-09 17:27:51 +1000

    Update unifi-install.sh

--- install/unifi-install.sh
+++ install/unifi-install.sh
@@ -32,7 +32,7 @@ msg_info "Installing MongoDB (v4.4)"
 wget -qO- https://www.mongodb.org/static/pgp/server-4.4.asc | gpg --dearmor >/usr/share/keyrings/mongodb-server-4.4.gpg
 echo "deb [ signed-by=/usr/share/keyrings/mongodb-server-4.4.gpg ] http://repo.mongodb.org/apt/debian bookworm/mongodb-org/4.4 main" >/etc/apt/sources.list.d/mongodb-org-4.4.list
 $STD apt-get update
-$STD apt-get install -y mongodb-org=4.4.29
+$STD apt-get install -y mongodb-org=4.4
 msg_ok "Installed MongoDB (v4.4)"
 
 msg_info "Installing UniFi Network Server"
